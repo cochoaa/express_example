c6c004bb180bea0cf294dbfabc06eb118b6bec3c
Carlos Ochoa <carlosoaly@gmail.com>
2021-03-11 11:33:15 -0500

    Configurando despliegue

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,6 @@ echo "Me muevo a app"
 cd app
 pwd
 ls -l
+
+rsync -avz  --delete -e "ssh" app/  node@prueba02.quipucamayoc.unmsm.edu.pe:/opt/deploy/prueba/
+ssh node@prueba02.quipucamayoc.unmsm.edu.pe "pm2 reload /opt/deploy/apps.yml --only prueba"
