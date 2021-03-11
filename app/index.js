e28c8b949eb09ca3b920b4a1f730e55e92771f3b
Carlos Ochoa <carlosoaly@gmail.com>
2021-03-10 19:51:23 -0500

    Agregado router

--- app/index.js
+++ app/index.js
@@ -1,15 +1,17 @@
 const express= require('express');
+var wiki = require('./routers/wiki.js');
 const PORT=parseInt(process.argv[2], 10);
 const PID=process.pid;
 const app=express();
 
+app.use('/wiki', wiki);
 
-app.get('/',function(req,res){
+/*app.get('/',function(req,res){
     var response=`<h1>Instancia de Node ejecutando </h1>`;
     response+=`<h2>Puerto: ${PORT} </h2> `;
     response+=`<h2>ProcessId: ${PID} </h2> `;
     res.end(response);
-});
+});*/
 
 app.listen(PORT,function(){
     console.log(`Sistema escuchando en el puerto ${PORT}`);
---
+++ app/routers/wiki.js
@@ -0,0 +1,23 @@
+// wiki.js - Wiki route module.
+
+var express = require('express');
+var router = express.Router();
+
+// Home page route.
+router.get('/', function (req, res) {
+    const PORT=parseInt(process.argv[2], 10);
+    const PID=process.pid;
+    let response=`<h1>Aplicacion de Prueba Wiki ejecutando </h1>`;
+    response+=`<h2>Puerto: ${PORT} </h2> `;
+    response+=`<h2>ProcessId: ${PID} </h2> `;
+    response+="Primer commit";
+    res.send(response);
+})
+
+// About page route.
+router.get('/about', function (req, res) {
+  res.send('About this wiki');
+})
+
+module.exports = router;
+
