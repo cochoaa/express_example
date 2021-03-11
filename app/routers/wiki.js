90a3877328460aab2ef4fb4f90a908d5719696dd
Carlos Ochoa <carlosoaly@gmail.com>
2021-03-11 11:30:21 -0500

    Agregando version

--- app/routers/wiki.js
+++ app/routers/wiki.js
@@ -7,10 +7,12 @@ var router = express.Router();
 router.get('/', function (req, res) {
     const PORT=process.env.PORT;
     const PID=process.pid;
+    const version=process.env.version;
     let response=`<h1>Aplicacion de Prueba Wiki ejecutando </h1>`;
     response+=`<h2>Puerto: ${PORT} </h2> `;
     response+=`<h2>ProcessId: ${PID} </h2> `;
-    response+="Primer commit";
+    response+=`<h2>Version: ${version} </h2> `;
+    response+="Tercer commit";
     res.send(response);
 })
 
