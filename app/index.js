4c608322cef120d27143708447d1d88c39e06fe6
Carlos Ochoa <carlosoaly@gmail.com>
2022-10-31 16:04:06 -0500

    Configuracion de valores por defecto cuando no hay variables de entorno

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 const express= require('express');
 var wiki = require('./routers/wiki.js');
-//const PORT=parseInt(process.argv[2], 10);
-const PORT=process.env.PORT;
-const CONTEXT=process.env.CONTEXT;
+const PORT_ARG=parseInt(process.argv[2], 10);
+const PORT=process.env.PORT||PORT_ARG;
+const CONTEXT=process.env.CONTEXT||'/';
 const PID=process.pid;
 const app=express();
 
